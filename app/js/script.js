6b4f8eb3b1d66bddbd95dd3015dcd42ab4d8f426
VyacheslavReznikov <lycansth@list.ru>
2020-07-14 19:13:26 +1000

    fix sliders arrows

--- app/js/script.js
+++ app/js/script.js
@@ -1,14 +1,27 @@
 $(function() {
 
 	$('.weekly__inner').slick({		
+		prevArrow: '<button type="button" class="weekly-arrow slick-arrow slick-prev lnr lnr-chevron-left">Previous</button>',
+		nextArrow: '<button type="button" class="weekly-arrow slick-arrow slick-next lnr lnr-chevron-right">Next</button>',
 		slidesToShow: 1,
 		appendArrows: $('.weekly__arrows'),
 	})
 
 	$('.followers__inner').slick({		
+		prevArrow: '<button type="button" class="followers-arrow slick-arrow slick-prev lnr lnr-chevron-left">Previous</button>',
+		nextArrow: '<button type="button" class="followers-arrow slick-arrow slick-next lnr lnr-chevron-right">Next</button>',
 		slideToScroll: 1,
 		slidesToShow: 3,
 		appendArrows: $('.followers__arrows'),
+	})
+
+	$('.clients__inner').slick({
+		prevArrow: '<button type="button" class="clients-arrow slick-arrow slick-prev lnr lnr-chevron-left">Previous</button>',
+		nextArrow: '<button type="button" class="clients-arrow slick-arrow slick-next lnr lnr-chevron-right">Next</button>',
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		arrows: true,
+		// centerMode: true,
 	})
 
 
