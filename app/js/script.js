c2dc8ab56b0080c8e70f46b616b775e338bc8dca
VyacheslavReznikov <lycansth@list.ru>
2020-07-19 21:41:47 +1000

    first page + adaptive

--- app/js/script.js
+++ app/js/script.js
@@ -12,7 +12,21 @@ $(function() {
 		nextArrow: '<button type="button" class="followers-arrow slick-arrow slick-next lnr lnr-chevron-right">Next</button>',
 		slideToScroll: 1,
 		slidesToShow: 3,
-		variableWidth: true,
+		// variableWidth: true,
+		responsive: [
+			{
+				breakpoint: 1121,
+				settings: {
+					slidesToShow: 2,				
+				}
+			},
+			{
+				breakpoint: 769,
+				settings: {
+					slidesToShow: 1
+				}
+			}
+		],
 
 		appendArrows: $('.followers__arrows')
 
@@ -23,7 +37,23 @@ $(function() {
 		nextArrow: '<button type="button" class="clients-arrow slick-arrow slick-next lnr lnr-chevron-right">Next</button>',
 		slidesToShow: 2,
 		slidesToScroll: 1,
-		variableWidth: true
+		// variableWidth: true.
+		responsive: [
+			{
+				breakpoint: 1200,
+				settings: {
+					appendArrows: $('.clients__arrows')
+				}
+			},
+			{
+				breakpoint: 769,
+				settings: {
+					slidesToShow: 1,
+					appendArrows: $('.clients__arrows')
+
+				}
+			}
+		],
 
 	})
 
@@ -35,6 +65,19 @@ $(function() {
 		ratedFill: '#ffc000',
 		readOnly: true,
 
+	})
+
+
+	$('.header__btn-menu').on('click', function() {
+		$('.header__items').toggleClass('active')
+	})
+
+	$('.menu__burger').on('click', function() {
+		$('.menu__list').slideToggle()
+	})
+
+	$('.menu__item--dropdown').on('click', function() {
+		$('.dropdown__menu--left').slideToggle()
 	})
 
 
