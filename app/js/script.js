8a6f4fa9ff7af688527fa3ae8f70f5bf2a9d1af4
VyacheslavReznikov <lycansth@list.ru>
2020-11-22 15:53:34 +1000

    перезаливка

--- app/js/script.js
+++ app/js/script.js
@@ -106,8 +106,6 @@ $(function() {
 
 	$('.aside__title').on('click', function() {
 		$(this).next('.aside__list').slideToggle()
-
-
 	})
 
 
@@ -133,6 +131,15 @@ $(function() {
 		to: 300,
 		prefix: "$"
 	})
+
+	$('.single-product__tabs .tab').on('click', function(event) {
+		var id = $(this).attr('data-id');
+			$('.single-product__tabs').find('.tab-item').removeClass('active-tab').hide();
+			$('.single-product__tabs .tabs').find('.tab').removeClass('active');
+			$(this).addClass('active');
+			$('#'+id).addClass('active-tab').fadeIn();
+			return false;
+		});
 
 
 	if ($('.release__wrapper').length) {
